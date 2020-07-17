566a34c89ddddef1f5350d0c25e78bf88282a12c
Hôih My <my.hoih@axonactive.com>
2020-07-17 11:27:38 +0700

    Add snake game

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,14 @@
 print("Hello World!")
+
+a = "Hello"
+b = " World!!!!!"
+c = "true"
+x = 2
+
+print('good' not in 'this is a greate example' )
+
+fruits = ['banana','apple','mango']
+print ('Current fruit :', fruits)
+
+mytuple = ('x', 'y', 'z')
+print(mytuple)
